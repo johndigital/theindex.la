16e074186181bf1a399e17d910252be4cc78ed76
John Robson <j@hn.digital>
2024-03-07 15:52:27 -0500

    ip log

--- plugins/bootstrap.js
+++ plugins/bootstrap.js
@@ -3,10 +3,39 @@ import _kebabCase from 'lodash/kebabCase'
 import _camelCase from 'lodash/camelCase'
 import Vue from 'vue'
 
-export default async ({ store, route }, inject) => {
-    // external components
-    Vue.component('responsive-image', require('fh-components/responsive-image'))
-    Vue.directive('in-view', require('fh-components/v-in-view'))
+// external components
+Vue.component('responsive-image', require('fh-components/responsive-image'))
+Vue.directive('in-view', require('fh-components/v-in-view'))
+
+// globally register everything in the /components folder
+const components = require.context('~/components', true)
+components.keys().map(component => {
+    // turn './ComponentName.vue' into 'component-name'
+    const componentName = _kebabCase(
+        component.replace(/^\.\//, '').replace(/\.vue$/, '')
+    )
+
+    // register new component globally
+    Vue.component(componentName, components(component).default)
+})
+
+// globally register everything in the /filters folder
+const filters = require.context('~/filters', true)
+filters.keys().map(filter => {
+    // turn './ComponentName.vue' into 'component-name'
+    const filterName = _camelCase(
+        filter.replace(/^\.\//, '').replace(/\.js/, '')
+    )
+
+    // register new filter globally
+    Vue.filter(filterName, filters(filter).default)
+})
+
+export default async ({ store, route, req }) => {
+    if (process.server) {
+        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+        console.log('ip: ', ip)
+    }
 
     // preload global data
     const [
@@ -47,28 +76,4 @@ export default async ({ store, route }, inject) => {
     if (route.path !== '/about') {
         store.commit('SHOW_LOADING')
     }
-
-    // globally register everything in the /components folder
-    const components = require.context('~/components', true)
-    components.keys().map(component => {
-        // turn './ComponentName.vue' into 'component-name'
-        const componentName = _kebabCase(
-            component.replace(/^\.\//, '').replace(/\.vue$/, '')
-        )
-
-        // register new component globally
-        Vue.component(componentName, components(component).default)
-    })
-
-    // globally register everything in the /filters folder
-    const filters = require.context('~/filters', true)
-    filters.keys().map(filter => {
-        // turn './ComponentName.vue' into 'component-name'
-        const filterName = _camelCase(
-            filter.replace(/^\.\//, '').replace(/\.js/, '')
-        )
-
-        // register new filter globally
-        Vue.filter(filterName, filters(filter).default)
-    })
 }
